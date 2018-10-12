37401e3c3b8a91654fce0eabbc867614b792f9d8
glittika-gerdmanee <glittika.gerdmanee@gmail.com>
2018-10-12 11:27:29 +1100

    String array for InstructionScroll
    -Same as image scrolling but for text/strings instead

--- FireballProject/Assets/Scripts/InstructionScroll.cs
+++ FireballProject/Assets/Scripts/InstructionScroll.cs
@@ -6,10 +6,6 @@ using UnityEngine.UI;
 public class InstructionScroll : MonoBehaviour
 {
 
-
-    //reset the images to 0 so that when instruction is reopened
-    //the image display is always on the very first image and not the last opened image
-
     //array of images for Instructions panel
     public Sprite[] images = new Sprite[1];
 
@@ -19,6 +15,16 @@ public class InstructionScroll : MonoBehaviour
     //Image currently displayed
     public Image currentImage = null;
 
+    //array of strings for Instructions panel
+    [TextArea(3,5)]
+    public string[] texts = new string[1];
+
+    //current position in string array
+    public int textPos = 0;
+
+    //String currently displayed in Text
+    public Text currentText = null;
+
     //the right and left button respectively
     public Button nextButton = null;
     public Button prevButton = null;
@@ -26,30 +32,38 @@ public class InstructionScroll : MonoBehaviour
 
     public void RightButton()
     {
-        if (imagePos + 1 < images.Length)
+        if (imagePos + 1 < images.Length && textPos + 1 < texts.Length)
         {
             ++imagePos;
+            ++textPos;
         }
+
+
     }
 
     public void LeftButton()
     {
-        if (imagePos - 1 >= 0)
+        if (imagePos - 1 >= 0 && textPos - 1 >= 0)
         {
             --imagePos;
+            --textPos;
         }
+
+
     }
 
-    public void ResetImage()
+    public void ResetStep()
     {
-        if (imagePos >= 1)
+        if (imagePos >= 1 && textPos >= 1)
         {
             imagePos = 0;
+            textPos = 0;
         }
     }
 
     public void Update()
     {
         currentImage.sprite = images[imagePos];
+        currentText.text = texts[textPos];
     }
 }

4c78aad97c81bbd0a9f56855e437c92d1628b724
glittika-gerdmanee <glittika.gerdmanee@gmail.com>
2018-10-12 16:44:41 +1100

    Options Script in Progress
    -Music & SFX Volume work (so far)

---
+++ FireballProject/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+
+    //mian options panel
+    public GameObject optionsPanel;
+
+    //control mapping panel
+    public GameObject controlsPanel;
+
+    //sound options panel
+    public GameObject soundsPanel;
+
+    //video options panel
+    public GameObject videoPanel;
+
+    /*public Button applyButton = null;
+    public Button backButton = null;
+    public Button controlButton = null;
+    public Button soundButton = null;
+    public Button videoButton = null;*/
+    
+    //slider for bgm
+    public Slider musicVolume;
+
+    //slider for sfx
+    public Slider soundVolume;
+
+    //source of bgm
+    public AudioSource musicSource;
+
+    //source of sfx
+    public AudioSource soundSource;
+
+    public Slider brightness;
+
+
+	// Use this for initialization
+	void Awake()
+    {
+        musicSource.Play();
+        Screen.fullScreen = true;
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+        AudioController();
+	}
+
+    //allows for audio to change volume
+    public void AudioController()
+    {
+        musicSource.volume = musicVolume.value;
+        //soundSource.volume = soundVolume.value;
+        //maybe have some numbers on the slider to indicate volume placement later
+        //playerprefs?
+    }
+
+    //switch between windowed mode and full screen
+    public void ScreenToggle()
+    {
+
+    }
+
+    //toggle brightness of game screen
+    public void BrightnessToggle()
+    {
+
+    }
+
+    //allowing panels to returning to options panel instead of closing it entirely
+    public void BackButton()
+    {
+       
+    }
+
+}

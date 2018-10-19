0c45f3c6f65cd9fd4cfc7fedd66bcac94766529d
glittika-gerdmanee <glittika.gerdmanee@gmail.com>
2018-10-19 11:01:01 +1100

    Options Menu Updated
    -Graphics quality change available
    -Screen resolution toggle still in progress

--- FireballProject/Assets/Scripts/OptionsMenu.cs
+++ FireballProject/Assets/Scripts/OptionsMenu.cs
@@ -24,28 +24,52 @@ public class OptionsMenu : MonoBehaviour {
     //button for player to return to other panels and leave options menu
     // public Button backButton = null;
 
-    
-    //slider for bgm
-    public Slider musicVolume;
-
-    //slider for sfx
-    public Slider soundVolume;
-
     //source of bgm
     public AudioSource musicSource;
 
     //source of sfx
     public AudioSource soundSource;
 
+    //slider for bgm
+    public Slider musicVolume;
+
+    //slider for sfx
+    public Slider soundVolume;
+
+
     public Slider brightness;
+
 
     public float brightnessLevel;
 
+    public Dropdown resolutionDropdown;
+
+    public Resolution[] resolution;
+
 	// Use this for initialization
-	void Awake()
+	void Start()
     {
-        musicSource.Play();
-        Screen.fullScreen = true;
+        resolution = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolution.Length; i++)
+        {
+            string resolutionOption = resolution[i].width + " x " + resolution[i].height;
+            options.Add(resolutionOption);
+
+            if(resolution[1].width == Screen.width && resolution[1].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 	
 	// Update is called once per frame
@@ -63,11 +87,25 @@ public class OptionsMenu : MonoBehaviour {
         //playerprefs?
     }
 
-    //switch between windowed mode and full screen
-    public void ScreenToggle()
+    //updates resolution
+    public void ScreenToggle(int resolutionIndex)
     {
-
+        Resolution resolutionScreen = resolution[resolutionIndex];
+        Screen.SetResolution(resolutionScreen.width, resolutionScreen.height, Screen.fullScreen, resolutionScreen.refreshRate);
     }
+
+    //displays as fullscreen
+    public void Fullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    //toggle between grahpics quality
+    public void GraphicsToggle(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    
 
     //toggle brightness of game screen
     public void BrightnessToggle()

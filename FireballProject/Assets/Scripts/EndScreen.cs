39ed7875f10dd14dddb23cd380b2ed8658b864b1
FinnPerry <finnperry8@gmail.com>
2018-11-01 13:36:45 +1100

    Score screen now displays scores in descending order

--- FireballProject/Assets/Scripts/EndScreen.cs
+++ FireballProject/Assets/Scripts/EndScreen.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Linq;
 
 public class EndScreen : MonoBehaviour
 {
@@ -59,20 +60,61 @@ public class EndScreen : MonoBehaviour
     // updates text to display scores
     public void UpdateUI()
     {
+        // get sorted score list
+        SortScores();
+
+        // disable text objects
         for (int i = 0; i < displayText.Length; ++i)
         {
-            // string to display
-            string str = "Player " + (i + 1).ToString() + ": ";
+            displayText[i].gameObject.SetActive(false);
+        }
 
-            // does this player key exist
-            if (scores.ContainsKey(i))
+        // set text and enable text objects
+        int textIndex = 0;
+        foreach (int key in scores.Keys)
+        {
+            // string to display
+            string str = "Player " + (key + 1).ToString() + " : " + scores[key].ToString();
+
+            // set ui text
+            displayText[textIndex].text = str;
+
+            // enable text
+            displayText[textIndex].gameObject.SetActive(true);
+
+            // next text field
+            ++textIndex;
+        }
+    }
+
+    // sorts the scores list
+    private static void SortScores()
+    {
+        // new list
+        Dictionary<int, int> newScores = new Dictionary<int, int>();
+
+        while (scores.Count > 0)
+        {
+            // find the highest value
+            int highestValue = 0;
+            int index = 0;
+            foreach (int key  in scores.Keys)
             {
-                // add score to string
-                str += (scores[i]).ToString();
+                if (scores[key] >= highestValue)
+                {
+                    highestValue = scores[key];
+                    index = key;
+                }
             }
 
-            // set UI text
-            displayText[i].text = str;
+            // add highest to new list
+            newScores.Add(index, highestValue);
+
+            // remove from old list
+            scores.Remove(index);
         }
+
+        // old list = new list
+        scores = newScores;
     }
 }

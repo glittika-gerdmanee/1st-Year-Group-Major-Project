2924618725a71cc8aa709e18a9673ddce87d1145
FinnPerry <finnperry8@gmail.com>
2018-11-08 16:37:31 +1100

    Removed redundant level select scene and refactored the level select script

--- FireballProject/Assets/Scripts/LevelSelect.cs
+++ FireballProject/Assets/Scripts/LevelSelect.cs
@@ -5,9 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour
 {
-	// load a scene
-    public void LoadScene(string sceneName)
+    // the selected scene
+    public static string currentScene = "";
+
+    // set the forest scene
+    public void SetForestLevel()
     {
-        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        currentScene = "Scenes/ForestLevel";
+
+        SceneManager.LoadScene("Scenes/CharacterSelect", LoadSceneMode.Single);
+    }
+
+    // set the ice scene
+    public void SetIceLevel()
+    {
+        currentScene = "Scenes/IceLevel";
+
+        SceneManager.LoadScene("Scenes/CharacterSelect", LoadSceneMode.Single);
     }
 }

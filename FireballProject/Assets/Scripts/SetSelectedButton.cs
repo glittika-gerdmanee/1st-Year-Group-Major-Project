8be4d12a9b627d3a2066d08f43f8bc53d568d36e
FinnPerry <finnperry8@gmail.com>
2018-11-22 14:34:58 +1100

    Menu now works properly with controller

---
+++ FireballProject/Assets/Scripts/SetSelectedButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SetSelectedButton : MonoBehaviour
+{
+	public void SetButton(Button button)
+    {
+        // get the event system
+        EventSystem eSys = FindObjectOfType<EventSystem>();
+
+        // set the selected object
+        eSys.SetSelectedGameObject(button.gameObject);
+    }
+
+    public void SetSlider(Slider slider)
+    {
+        // get the event system
+        EventSystem eSys = FindObjectOfType<EventSystem>();
+
+        // set the selected object
+        eSys.SetSelectedGameObject(slider.gameObject);
+    }
+
+    public void SetDropDown(Dropdown dropdown)
+    {
+        // get the event system
+        EventSystem eSys = FindObjectOfType<EventSystem>();
+
+        // set the selected object
+        eSys.SetSelectedGameObject(dropdown.gameObject);
+    }
+}

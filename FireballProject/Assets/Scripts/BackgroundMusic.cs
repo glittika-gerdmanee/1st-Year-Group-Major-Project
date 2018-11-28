9fe2dd187fc6dfa267cd651bcede2c5b345535a8
FinnPerry <finnperry8@gmail.com>
2018-11-28 11:34:32 +1100

    Added background music fade in-out

---
+++ FireballProject/Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    // time to fade in
+    public float fadeDuration = 0f;
+
+    // audio source
+    private AudioSource src = null;
+
+    private float elapsedTime = 0f;
+
+	// Use this for initialization
+	void Start()
+    {
+        // get source
+        src = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        float duration = GetComponent<GameTimer>().duration;
+        float remainingTime = duration - (elapsedTime - GetComponent<GameTimer>().startCountdownDuration);
+        if (elapsedTime < fadeDuration)
+        {
+            src.volume = Mathf.InverseLerp(0f, fadeDuration, elapsedTime);
+        }
+        else if (remainingTime < fadeDuration)
+        {
+            src.volume = Mathf.InverseLerp(0f, fadeDuration, remainingTime);
+        }
+        else
+        {
+            src.volume = 1f;
+        }
+	}
+}
